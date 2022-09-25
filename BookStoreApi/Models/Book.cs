f0ad7748463458d9b211c6282582239e82c1976f
thanhnm <nmthanh1306@gmail.com>
2022-09-25 23:22:42 +0700

    book store API first commit

---
+++ BookStoreApi/Models/Book.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json.Serialization;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace BookStoreApi.Models;
+
+public class Book
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+
+    [BsonElement("Name")]
+    [JsonPropertyName("Name")] // represent property name in the web API serialized JSON response.
+                               // Name instead of BookName
+    public string BookName { get; set; } = null!;
+
+    [BsonElement("Price")]
+    public decimal Price { get; set; }
+
+    public string Category { get; set; } = null!;
+
+    public string Author { get; set; } = null!;
+}
